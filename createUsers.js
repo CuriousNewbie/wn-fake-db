a483a98932ce16ad0b99f7f3e2a192c95d2bdd4c
PC <umjahongir@gmail.com>
2022-10-15 14:48:14 +0500

    Minor bugfix

--- createUsers.js
+++ createUsers.js
@@ -11,11 +11,11 @@ const bunchObjects = (n, objFactory) => {
   return Array.from(Array(n), (i, j) => objFactory());
 };
 
-const subsString = (n, rand) => {
-  const arr = Array.from(Array(n), (el) => rand());
+const subsString = (n, rand, id) => {
+  const arr = Array.from(Array(n), (el) => rand()).filter((el) => el !== id);
   return "[" + arr.join(",") + "]";
 };
-
+let id = 1;
 const createUser = () => {
   const rand = randomInts(1, users_num);
   const subsNum = Math.ceil(Math.random() * users_num * 0.4);
@@ -27,19 +27,21 @@ const createUser = () => {
   const logName = name.slice(0, 4) + lastName.slice(0, 4);
   const email = logName + "@example.com";
   const password = logName + "User";
-
-  return {
+  let data = {
+    id,
     name: logName,
     password: createHash("sha256").update(password).digest("hex"),
     avatar: avatarUrl + logName,
     email,
-    subs: subsString(subsNum, rand),
-    blocks: subsString(blocksNum, rand),
+    subs: subsString(subsNum, rand, id),
+    blocks: subsString(blocksNum, rand, id),
   };
+  id++;
+  return data;
 };
 
 const colSet = new pgp.helpers.ColumnSet(
-  ["name", "password", "avatar", "email", "subs", "blocks"],
+  ["id", "name", "password", "avatar", "email", "subs", "blocks"],
   {
     table: "users",
   }
